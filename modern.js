f7c4e4f6b143d0dbfb6f7d26cd9617abdde087f8
Edward Minasyan <mrEDitor@mail.ru>
2017-03-24 04:03:19 +0300

    Add Inscriptor page

--- modern.js
+++ modern.js
@@ -31,8 +31,54 @@ function setWallpaper() {
 	document.body.appendChild(wallpaper);
 }
 
+function handleModalable() {
+	const links = document.getElementsByClassName('modalable');
+	for(var i = 0; i < links.length; i++) {
+		links[i].onclick = function() {
+			showModal(this.href);
+			return false;
+		}
+	}
+}
+
+function fitModal(e) {
+	const scroller = document.getElementById('scroller');
+	const rotator = {
+		'fit': 'zoom',
+		'zoom': 'fit',
+	};
+	scroller.className = rotator[scroller.className];
+	e.stopPropagation();
+	return false;
+}
+
+function hideModal() {
+	const grayout = document.getElementById('grayout');
+	document.body.removeChild(grayout);
+	document.body.style.overflow = 'auto';
+	return false;
+}
+
+function showModal(url) {
+	const grayout = document.createElement('div');
+	grayout.id = 'grayout';
+	grayout.onclick = hideModal;
+	const scroller = document.createElement('div');
+	scroller.id = 'scroller';
+	scroller.className = 'fit';
+	const image = document.createElement('img');
+	image.onclick = fitModal;
+	image.src = url;
+	scroller.appendChild(image);
+	grayout.appendChild(scroller);
+	document.body.appendChild(grayout);
+	document.body.style.overflow = 'hidden';
+	return false;
+}
+
 const QUERY_COUNT = 'https://cloud-api.yandex.net:443/v1/disk/public/resources'
 	+ '?public_key=TtRkzUEDRX7oa0doBszPuib3Vok%2BZvro2fnE7gA9aHk%3D'
 	+ '&fields=_embedded.total';
 
 tryOptinalGetRequest(QUERY_COUNT, chooseWallpaper, 1000);
+document.addEventListener('DOMContentLoaded', handleModalable);

cbf049cd83ea81939b2d02248d1b96809c2319c0
Eduard Minasyan <mreditor@mail.ru>
2020-10-07 13:13:46 +0300

    Update modern.js

--- modern.js
+++ modern.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  if (location.hash == "print") {
+  if (location.hash == "#print") {
     var elements = document.querySelectorAll('[data-abbr]');
     for ( var i = 0; i < elements.length; i++ ) {
       elements[i].innerHTML = elements[i].dataset['abbr'];
